3f324ebaad1b8a22d8bf3539ff462de1b301469c
Norwill <willeverett206@gmail.com>
2025-10-23 11:31:53 -0400

    basic shop ui not anims
    added burger to pop up to big pop up

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,10 +4,27 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject shopMenu;
+    private bool isShopOpen = false;
+
     public void StartGame()
     {
         SceneManager.LoadScene("CaveWhitebox");
         Debug.Log("Start game.");
+    }
+
+    public void ToggleShopMenu()
+    {
+        if (isShopOpen)
+        {
+            shopMenu.SetActive(false);
+            isShopOpen = false;
+        }
+        else
+        {
+            shopMenu.SetActive(true);
+            isShopOpen = true;
+        }
     }
 }
 
