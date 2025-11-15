749a0d0941d1d1372e80e506817574ab6432039a
n0rwill <willeverett206@gmail.com>
2025-11-15 12:08:12 -0500

    made bulletinfo script

--- Assets/Scripts/PlantAttack.cs
+++ Assets/Scripts/PlantAttack.cs
@@ -1,16 +1,69 @@
+using System.Collections;
 using UnityEngine;
+
 
 public class PlantAttack : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+
+    [Header("Attack")]
+    public float attackDamage;
+    public float attackDelay;
+    public float attackRange;
+    public int bulletpierce;
+    public int bulletType;
+    public GameObject bulletPrefab;
+    private EnemyInfo enemy;
+
     void Update()
     {
-        
+        enemy = FindClosestEnemy();
     }
+
+    private EnemyInfo FindClosestEnemy()
+    {
+        EnemyInfo[] enemies = FindObjectsOfType<EnemyInfo>();
+        EnemyInfo closestEnemy = null;
+        float closestDistance = Mathf.Infinity;
+
+        foreach (EnemyInfo enemy in enemies)
+        {
+            float distance = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distance < closestDistance && distance <= attackRange)
+            {
+                closestDistance = distance;
+                closestEnemy = enemy;
+            }
+        }
+
+        return closestEnemy;
+    }
+
+    public IEnumerator AttackRoutine(EnemyInfo enemy)
+    {
+        while (enemy != null && Vector3.Distance(transform.position, enemy.transform.position) <= attackRange)
+        {
+            // Instantiate bullet and set its target
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+
+            switch (bulletType)
+            {
+                case 0:
+                    // Basic bullet logic
+                    BulletInfo bulletInfo = bullet.GetComponent<BulletInfo>();
+                    break;
+                case 1:
+                    // Piercing arking bullet logic
+
+                    break;
+                case 2:
+                    // Aoe bullet logic
+
+                    break;
+            }
+
+            yield return new WaitForSeconds(attackDelay);
+        }
+    }
+
 }
