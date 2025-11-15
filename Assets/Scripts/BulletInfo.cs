087f463fb873e250747ce2c26795a03006db194b
n0rwill <willeverett206@gmail.com>
2025-11-15 12:19:18 -0500

    started bullet script

--- Assets/Scripts/BulletInfo.cs
+++ Assets/Scripts/BulletInfo.cs
@@ -1,16 +1,73 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class BulletInfo : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    public float speed = 10f;
+    private Transform target;
+    private float damage;
+    private int pierce;
+    private int type;
+    public float bulletDuration;
 
+    public void Start()
+    {
+        // Optionally, you can add a self-destruct timer to prevent bullets from existing indefinitely
+        Destroy(gameObject, bulletDuration);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetTarget(Transform target, float damage, int pierce, int type)
     {
+        this.target = target;
+        this.damage = damage;
+        this.pierce = pierce;
+        this.type = type;
+    }
 
+    private void Update()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        transform.position += direction * speed * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, target.position) < 0.1f)
+        {
+            EnemyInfo enemyInfo = target.GetComponent<EnemyInfo>();
+            if (enemyInfo != null)
+            {
+                if (type == 0) // Basic bullet logic
+                {
+                    enemyInfo.health -= damage;
+                    pierce--;
+
+                    if (pierce <= 0)
+                    {
+                        Destroy(gameObject);
+                    }
+                }
+                else if (type == 1) // Piercing arking bullet logic
+                {
+                    enemyInfo.health -= damage;
+                    pierce--;
+
+                    if (pierce <= 0)
+                    {
+                        Destroy(gameObject);
+                    }
+                }
+                else if (type == 2) // AOE bullet logic
+                {
+                    Collider[] hitColliders = Physics.OverlapSphere(transform.position, 2f);
+                    foreach (var hitCollider in hitColliders)
+                    {
+                        EnemyInfo nearbyEnemy = hitCollider.GetComponent<EnemyInfo>();
+                        if (nearbyEnemy != null)
+                        {
+                            nearbyEnemy.health -= damage;
+                        }
+                    }
+                    Destroy(gameObject);
+                }
+            }
+        }
     }
 }
